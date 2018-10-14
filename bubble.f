91b0a78cd9bea8c080aa49989a450f7dd6056371
Roberto Villegas-Diaz <bthillo@hotmail.com>
2018-10-14 16:12:00 -0500

    Adding subroutines for SORT

--- bubble.f
+++ bubble.f
@@ -1,19 +1,10 @@
       PROGRAM BUBBLESORT
-      INTEGER ARR(10),I,J,K,SWAPP
-      CALL INPUT(ARR,10)
-      DO 2 K=1,10,1
-           J = 0
-           DO 3 I=1,9,1
-                IF(ARR(I+1).LT.ARR(I)) THEN
-		      CALL SWAP(ARR,10,I)
-                ELSE
-                     J = J + 1
-                END IF
-3          CONTINUE
-           IF(J.EQ.9)EXIT
-2     CONTINUE
+      INTEGER ARR(5),N
+      N = 5
+      CALL INPUT(ARR,N)
+      CALL SORT(ARR,N)
       PRINT *,'THE SORTED LIST IS: '
-      CALL OUTPUT(ARR,10)
+      CALL OUTPUT(ARR,N)
       END  
       
       SUBROUTINE INPUT(ARR,N)
@@ -29,6 +20,25 @@
            DO 4 I=1,N
                 PRINT *,ARR(I)
 4	   CONTINUE
+           RETURN
+      END
+      
+      SUBROUTINE SORT(ARR,N)
+	   INTEGER ARR(N),I,J,K,L,N
+           L = 0      
+	   DO 2 K=1,N
+                J = 0
+                DO 3 I=1,N-1
+                     IF(ARR(I+1).LT.ARR(I)) THEN
+                          CALL SWAP(ARR,N,I)
+                          L = L + 1
+                     ELSE
+                          J = J + 1
+                     END IF
+3               CONTINUE
+                IF(J.EQ.N-1)EXIT
+2          CONTINUE
+           PRINT *, 'TOTAL NUMBER OF SWAPS: ',L
            RETURN
       END
 
